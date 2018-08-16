6d6082f0c902666db7c26a0903b8acc4b29b73c8
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-08-16 11:20:22 -0700

    Update Nernst.py

--- Nernst.py
+++ Nernst.py
@@ -60,6 +60,21 @@ class Nernst():
         return membranePotential
 
 
+    def viz(self, volts):
+        """
+        :param volts: Volage in mV
+        :return: none
+        """
+        plt.scatter([0,1], [0, volts])
+        plt.plot([0,1], [0, volts])
+        plt.ylim(-100,100)
+        plt.xlim(0,2)
+        plt.xticks(np.arange(0, 2, 1.0))
+        plt.title("Membrane Potential Single Ion Species")
+        plt.xlabel("Time")
+        plt.ylabel("Voltage (mV")
+        plt.show()
+
 if __name__ == "__main__":
     Nernst = Nernst()
     temperature = Nernst.temp()
@@ -68,4 +83,5 @@ if __name__ == "__main__":
     outer = Nernst.outerConcentration()
     membrane = Nernst.equilibrium(temperature, charge, inner, outer)
     print("Result: " + str(membrane))
+    Nernst.viz(membrane*1000)
 
